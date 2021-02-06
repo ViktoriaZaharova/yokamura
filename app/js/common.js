ccdac95e54f8a11882b18e895743d19646a1a9d0
Viktoria <zaharova181114@gmail.com>
2021-02-06 07:55:13 +0200

    add pages

--- app/js/common.js
+++ app/js/common.js
@@ -147,3 +147,46 @@ $(document).on('click', function (e) {
         );
     }
 });
+
+// Инициализация карты
+ymaps.ready(init);
+
+function init () {
+
+    //Центрирование и выбор масштаба карты
+    let myMap = new ymaps.Map('map', {
+        center: [51.669470, 39.205407],
+        zoom: 16
+    });
+
+    // Создание своей метки
+    let myPlacemark = new ymaps.Placemark(
+        // Координаты метки
+        [51.669470, 39.205407] , {
+            // Свойства метки
+            hintContent: '', //Подсказка при наведении на маркер
+            iconContent: '',
+
+        }, {
+            iconImageHref: 'img/location.svg',  // картинка иконки
+            iconImageSize: [63, 91],                                      // размеры картинки
+            // iconImageOffset: [-70, -40],// смещение картинки
+
+        });
+
+    // Добавление метки на карту
+    myMap.geoObjects.add(myPlacemark);
+
+    //Элементы управления
+    myMap.controls
+    // Кнопка изменения масштаба
+        .add('zoomControl')
+        // Список типов карты
+        .add('typeSelector')
+        // Кнопка изменения масштаба - справа
+        .add('smallZoomControl', { right: 5, top: 75 })
+        // Стандартный набор кнопок
+        .add('mapTools')
+        //Линейка масштаба
+        .add(new ymaps.control.ScaleLine());
+}
