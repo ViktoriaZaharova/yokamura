a4efd33a8b12892d2919ec5169ded09246413157
Viktoria <zaharova181114@gmail.com>
2021-02-07 10:17:04 +0200

    add page basket

--- app/js/common.js
+++ app/js/common.js
@@ -148,6 +148,23 @@ $(document).on('click', function (e) {
     }
 });
 
+// amount
+$('.down').on("click", function () {
+    let $input = $(this).parent().find('input');
+    let count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+$('.up').on("click",function () {
+    let $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
+
+
 // Инициализация карты
 ymaps.ready(init);
 
