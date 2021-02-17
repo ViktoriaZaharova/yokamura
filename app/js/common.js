2b640650bca859c225fa230d52d813b0b43b849b
Viktoria <zaharova181114@gmail.com>
2021-02-17 18:27:25 +0200

    edit slider 2

--- app/js/common.js
+++ app/js/common.js
@@ -149,9 +149,101 @@ $(window).on('load resize', function() {
 });
 // slick active
 
-$('.news-slider').slick({
-    slidesToShow: 5,
-    centerMode: true,
+// $('.news-slider').slick({
+//     slidesToShow: 5,
+//     variableWidth: true,
+//     easing: 'ease',
+//     centerMode: true,
+//     nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="15.001" height="25" viewBox="0 0 15.001 25">\n' +
+//         '  <g transform="translate(-3789.771 -2184.9)">\n' +
+//         '    <path d="M3792.272,2184.9a2.489,2.489,0,0,1,1.767.733l10,10a2.5,2.5,0,0,1,0,3.535l-10,10a2.5,2.5,0,0,1-3.535-3.535l8.232-8.233-8.232-8.232a2.5,2.5,0,0,1,1.768-4.268Z"/>\n' +
+//         '  </g>\n' +
+//         '</svg>\n</button>',
+//     prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="15.001" height="25" viewBox="0 0 15.001 25">\n' +
+//         '  <path d="M3774.272,2209.9a2.493,2.493,0,0,1-1.768-.732l-10-10a2.5,2.5,0,0,1,0-3.535l10-10a2.5,2.5,0,0,1,3.535,3.535l-8.232,8.232,8.232,8.233a2.5,2.5,0,0,1-1.767,4.267Z" transform="translate(-3761.771 -2184.9)"/>\n' +
+//         '</svg>\n</button>',
+//     responsive: [
+//         {
+//             breakpoint: 992,
+//             settings: {
+//                 slidesToShow: 3,
+//             }
+//         },
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 variableWidth: true,
+//             }
+//         }
+//     ]
+// });
+//
+//
+//
+// //Добавляем всем элементам прозрачность
+// !$('.news-slider .slick-slide').not('.news-slider .slick-center').addClass('slick-slider-opacity');
+// //Убираем прозрачность у центральных элементов
+// $('.news-slider .slick-slide.slick-active').eq(0).removeClass('slick-slider-opacity');
+// $('.news-slider .slick-slide.slick-active').eq(4).removeClass('slick-slider-opacity');
+// // $('.news-slider .slick-slide.slick-active').eq(4).removeClass('slick-slider-opacity');
+// // // Вешаем обработчик на событие слайдера
+// $(".news-slider").on("afterChange", function() {
+//     //Добавляем прозрачность всем элементам, обнуляя её у нужных
+//     $('.news-slider .slick-slide').not('.news-slider .slick-center').addClass('slick-slider-opacity');
+//     $('.news-slider .slick-slide.slick-active').eq(0).removeClass('slick-slider-opacity');
+//     // $('.news-slider .slick-slide.slick-active').eq(2).removeClass('slick-slider-opacity');
+//     $('.news-slider .slick-slide.slick-active').eq(4).removeClass('slick-slider-opacity');
+// });
+
+let rev = $(".news-slider");
+rev
+    .on("init", function (event, slick, currentSlide) {
+        let cur = $(slick.$slides[slick.currentSlide]),
+            next = cur.next(),
+            next2 = cur.next().next(),
+            prev = cur.prev(),
+            prev2 = cur.prev().prev();
+        prev.addClass("slick-sprev");
+        next.addClass("slick-snext");
+        prev2.addClass("slick-sprev2");
+        next2.addClass("slick-snext2");
+        cur
+            .removeClass("slick-snext")
+            .removeClass("slick-sprev")
+            .removeClass("slick-snext2")
+            .removeClass("slick-sprev2");
+        slick.$prev = prev;
+        slick.$next = next;
+    })
+    .on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+        console.log("beforeChange");
+        let cur = $(slick.$slides[nextSlide]);
+        console.log(slick.$prev, slick.$next);
+        slick.$prev.removeClass("slick-sprev");
+        slick.$next.removeClass("slick-snext");
+        slick.$prev.prev().removeClass("slick-sprev2");
+        slick.$next.next().removeClass("slick-snext2");
+        (next = cur.next()), (prev = cur.prev());
+        //prev2.prev().prev();
+        //next2.next().next();
+        prev.addClass("slick-sprev");
+        next.addClass("slick-snext");
+        prev.prev().addClass("slick-sprev2");
+        next.next().addClass("slick-snext2");
+        slick.$prev = prev;
+        slick.$next = next;
+        cur
+            .removeClass("slick-next")
+            .removeClass("slick-sprev")
+            .removeClass("slick-next2")
+            .removeClass("slick-sprev2");
+    });
+
+rev.slick({
+    speed: 1000,
+    arrows: true,
+    dots: false,
+    // focusOnSelect: true,
     nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="15.001" height="25" viewBox="0 0 15.001 25">\n' +
         '  <g transform="translate(-3789.771 -2184.9)">\n' +
         '    <path d="M3792.272,2184.9a2.489,2.489,0,0,1,1.767.733l10,10a2.5,2.5,0,0,1,0,3.535l-10,10a2.5,2.5,0,0,1-3.535-3.535l8.232-8.233-8.232-8.232a2.5,2.5,0,0,1,1.768-4.268Z"/>\n' +
@@ -160,21 +252,19 @@ $('.news-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="15.001" height="25" viewBox="0 0 15.001 25">\n' +
         '  <path d="M3774.272,2209.9a2.493,2.493,0,0,1-1.768-.732l-10-10a2.5,2.5,0,0,1,0-3.535l10-10a2.5,2.5,0,0,1,3.535,3.535l-8.232,8.232,8.232,8.233a2.5,2.5,0,0,1-1.767,4.267Z" transform="translate(-3761.771 -2184.9)"/>\n' +
         '</svg>\n</button>',
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 3,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                variableWidth: true,
-            }
-        }
-    ]
+    infinite: true,
+    centerMode: true,
+    slidesPerRow: 1,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerPadding: "0",
+    swipe: true,
+    customPaging: function (slider, i) {
+        return "";
+    }
+    /*infinite: false,*/
 });
+
 
 // product slider
 $('.features-slider').slick({
@@ -249,20 +339,7 @@ $(".product-gallery").on("afterChange", function() {
     $('.product-gallery .slick-slide.slick-active').eq(3).removeClass('slick-slider-my-style');
 });
 
-//Добавляем всем элементам прозрачность
-$('.news-slider .slick-slide').addClass('slick-slider-opacity');
-//Убираем прозрачность у центральных элементов
-$('.news-slider .slick-slide.slick-active').eq(0).removeClass('slick-slider-opacity');
-$('.news-slider .slick-slide.slick-active').eq(2).removeClass('slick-slider-opacity');
-$('.news-slider .slick-slide.slick-active').eq(4).removeClass('slick-slider-opacity');
-//Вешаем обработчик на событие слайдера
-$(".news-slider").on("afterChange", function() {
-    //Добавляем прозрачность всем элементам, обнуляя её у нужных
-    $('.news-slider .slick-slide').addClass('slick-slider-opacity');
-    $('.news-slider .slick-slide.slick-active').eq(0).removeClass('slick-slider-opacity');
-    $('.news-slider .slick-slide.slick-active').eq(2).removeClass('slick-slider-opacity');
-    $('.news-slider .slick-slide.slick-active').eq(4).removeClass('slick-slider-opacity');
-});
+
 
 // menu catalog
 $('.links-catalog, .btn-burger').on("click", function (e) {
